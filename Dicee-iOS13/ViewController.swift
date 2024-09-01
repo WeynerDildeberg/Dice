81a815de5d8d0c5edb3d666e55e6fc75e6432092
Weyner Dildeberg <weynerdjs@192.168.100.23>
2024-09-01 09:59:02 -0300

    add project dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,11 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Integracao imagel 
+        diceImageView1.image = UIImage.init(named: "DiceSix")
+        
+        diceImageView2.image = UIImage(named: "DiceTwo")
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView2.alpha = 0.5
+        
+        
+    
     }
+    
 
 
 }
